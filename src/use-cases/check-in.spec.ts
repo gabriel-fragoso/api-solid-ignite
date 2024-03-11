7e2a7abe4e64c500ca8343942091dff740294b19
Gabriel F. de Oliveira <fragosooliveira773@gmail.com>
2024-03-11 13:58:53 -0300

    Adiciona dependência dayjs, implementa validação de distância máxima e número máximo de check-ins

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,24 +1,107 @@
-import { expect, describe, it, beforeEach } from "vitest";
-import { RegisterUseCase } from "./register";
-import { compare } from "bcryptjs";
+import { expect, describe, it, beforeEach, vi, afterEach } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { CheckInUseCase } from "./check-in";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gym-repository";
+import { Decimal } from "@prisma/client/runtime/library";
+import { MaxNumberOfCheckInsError } from "./errors/max-number-of-check-ins-error";
+import { MaxDistanceError } from "./errors/max-distance-error";
 
 let checkInRepository: InMemoryCheckInsRepository;
+let gymsRepository: InMemoryGymsRepository;
 let sut: CheckInUseCase;
 
 describe("Check-in Use Case", () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     checkInRepository = new InMemoryCheckInsRepository();
-    sut = new CheckInUseCase(checkInRepository);
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CheckInUseCase(checkInRepository, gymsRepository);
+
+    vi.useFakeTimers();
+
+    await gymsRepository.create({
+      id: "gym-01",
+      title: "JavaScript Gym",
+      description: "The best gym for JavaScript developers",
+      phone: "83 998304284",
+      latitude: -7.059866,
+      longitude: -36.363289,
+    });
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
   });
 
   it("should be able to check in", async () => {
     const { checkIn } = await sut.execute({
       gymId: "gym-01",
       userId: "user-01",
+      userLatitude: -7.059866,
+      userLongitude: -36.363289,
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to check in twice in the same day", async () => {
+    vi.setSystemTime(new Date(2022, 0, 15, 8, 0, 0));
+
+    await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: -7.059866,
+      userLongitude: -36.363289,
+    });
+
+    await expect(() =>
+      sut.execute({
+        gymId: "gym-01",
+        userId: "user-01",
+        userLatitude: -7.059866,
+        userLongitude: -36.363289,
+      })
+    ).rejects.toBeInstanceOf(MaxNumberOfCheckInsError);
+  });
+
+  it("should not be able to check in twice but in different days", async () => {
+    vi.setSystemTime(new Date(2022, 0, 15, 8, 0, 0));
+
+    await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: -7.059866,
+      userLongitude: -36.363289,
+    });
+
+    vi.setSystemTime(new Date(2022, 0, 16, 8, 0, 0));
+
+    const { checkIn } = await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: -7.059866,
+      userLongitude: -36.363289,
+    });
+
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to check in on distant gym", async () => {
+    gymsRepository.items.push({
+      id: "gym-02",
+      title: "JavaScript Gym",
+      description: "The best gym for JavaScript developers",
+      phone: "83 998304284",
+      latitude: new Decimal(-7.0729947),
+      longitude: new Decimal(-36.3638526),
+    });
+
+    await expect(() =>
+      sut.execute({
+        gymId: "gym-02",
+        userId: "user-01",
+        userLatitude: -7.059866,
+        userLongitude: -36.363289,
+      })
+    ).rejects.toBeInstanceOf(MaxDistanceError);
   });
 });
---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { expect, describe, it, beforeEach } from "vitest";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gym-repository";
+import { CreateGymUseCase } from "./create-gym";
+
+let gymsRepository: InMemoryGymsRepository;
+let sut: CreateGymUseCase;
+
+describe("Regiser Use Case", () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CreateGymUseCase(gymsRepository);
+  });
+
+  it("should be able to create gym", async () => {
+    const { gym } = await sut.execute({
+      title: "JavaScript Gym",
+      description: null,
+      phone: null,
+      latitude: -7.059866,
+      longitude: -36.363289,
+    });
+
+    expect(gym.id).toEqual(expect.any(String));
+  });
+});
